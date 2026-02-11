f271aa860d6f60fa7e22831ed9b72072bf37f791
Tae0072 <rkdxodh41@gmail.com>
2026-02-11 15:47:02 +0900

    폴링

---
+++ src/ex07/polling/App.java
@@ -0,0 +1,41 @@
+package ex07.polling;
+
+public class App {
+    public static void main(String[] args) {
+        LotteMart lm = new LotteMart();
+        Custormer1 cus1 = new Custormer1();
+
+        //1. 마트 입고 준비
+        new Thread(() -> {
+            // 10초 대기
+            for (int i = 1; i < 11; i++) {
+                System.out.println("입고중....(" +i+ "초)");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            // 입고 완료
+            lm.received();
+            System.out.println("입고 완료.");
+        }).start();
+
+        //2. 손님이 폴링
+        new Thread(() -> {
+            // 전화를 3초에 한번씩 하는것
+            while (true){
+                System.out.println("[손님]상품있어......?");
+                cus1.request(lm);
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        }).start();
+
+
+    }
+}
---
+++ src/ex07/push/App.java
@@ -0,0 +1,13 @@
+package ex07.push;
+
+/**
+ * push 방식 (옵저버 패턴)
+ * 1. 구현 방식이 정해져 있음.
+ * 2. 자바 lib: Reactive Java (자바 9), Stomp(그 외)
+ * 3. 스프링 : Stomp (WebSocket 라이브러리)
+ */
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
