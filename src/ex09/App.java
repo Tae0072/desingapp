740f286066f0d2b91792bd955f00e226493b91a7
Tae0072 <rkdxodh41@gmail.com>
2026-02-12 11:01:14 +0900

    펙토리

--- src/ex09/App.java
+++ src/ex09/App.java
@@ -1,8 +1,18 @@
 package ex09;
 
+import ex09.lib.DB;
+import ex09.lib.Driver;
+
 /**
  * 팩토리 패턴 예제
  */
 
 public class App {
-}
+    public static void main(String[] args) {
+
+            DBFactory factory = DBFactory.getInstance();
+            DB db = factory.createDB(Driver.MARIA);   // 팩토리에게 요청!
+            db.execute("select");
+        }
+    }
+
